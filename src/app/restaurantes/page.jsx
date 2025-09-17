78644f90a53b02b30f0bd8b826dd5f14f9095e74
Mateus Rodrigo <mateus.rodrigo@aluno.ifsp.edu.br>
2025-09-17 16:54:24 -0300

    att

--- src/app/restaurantes/page.jsx
+++ src/app/restaurantes/page.jsx
@@ -6,6 +6,11 @@ import Footer from "@/components/Footer";
 import RestaurantesLista from "@/components/Restaurantes/Lista";
 
 export default function RestaurantesPage() {
+    const restaurantes = [
+    { id: 1, nome: "Restaurante A", tipo: "Comida Brasileira", endereco: "Rua 1, Centro" },
+    { id: 2, nome: "Restaurante B", tipo: "Italiana", endereco: "Av. Principal, 123" },
+    { id: 3, nome: "Restaurante C", tipo: "Fast Food", endereco: "Praça Central, 45" },
+  ];
   return (
     <div>
       <Header />
