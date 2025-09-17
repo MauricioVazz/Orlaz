e787f4dc0f1ce422eea44f6a46be16b1db9954a6
MauricioVazz <mauricio.vaz@aluno.ifsp.edu.br>
2025-09-17 15:52:13 -0300

    section gastronomia implementada em todas as paginas de cidades😎🥶

--- src/app/Ilhabela/page.jsx
+++ src/app/Ilhabela/page.jsx
@@ -9,6 +9,7 @@ import Footer from "@/components/Footer";
 import AtracoesBela from "@/components/Atracoes/Bela"; // Import corrigido para o nome correto
 import { FaWater, FaSailboat, FaUmbrellaBeach, FaDrum } from "react-icons/fa6";
 import EventosTimeline from "@/components/EventosTimeline";
+import Gastronomy from "@/components/Gastronomy";
 
 export default function Ilhabela() {
   const eventos = [
@@ -35,9 +36,42 @@ export default function Ilhabela() {
         mapaUrl="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d117435.23417285034!2d-45.459!3d-23.815!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cd63bfbcc6d16f%3A0x6e1e90d5d7e081e4!2sIlhabela%2C%20SP!5e0!3m2!1spt-BR!2sbr!4v1694460956754!5m2!1spt-BR!2sbr"
       />
       <AtracoesBela />
-      
-       {/* Aqui entram os eventos */}
-       <EventosTimeline eventos={eventos} cor="#ec7702ff" />
+
+      <Gastronomy
+        title="Gastronomia"
+        subtitle="Comidas típicas da região"
+        items={[
+          {
+            name: "Azul marinho",
+            desc: "Um peixe típico refogado com banana nanica; o tanino na banana deixa o caldo com coloração azulada.",
+            image: "/images/azulmarinho.jpg",
+          },
+          {
+            name: "Peixe assado na folha de bananeira",
+            desc: "Peixe temperado com limão, alho e coentro, embrulhado na folha de bananeira e grelhado para exaltar o sabor e a tradição caiçara.",
+            image: "/images/peixe-bananeira.jpg",
+          },
+          {
+            name: "Moquecas e risotos de frutos do mar",
+            desc: "Moquecas, caldeiradas e risotos de peixe, lula, camarão e outros. Receitas preparadas com ervas regionais, leite, mandioca, azeite e dendê.",
+            image: "/images/moqueca-frutos.jpg",
+          },
+          {
+            name: "Caldeirada",
+            desc: "Preparada com peixe, lula, mariscos, batata, pimentão e cheiro-verde, a caldeirada é um ícone forte na culinária local.",
+            image: "/images/caldeirada.jpg",
+          },
+          {
+            name: "Destaques com camarão",
+            desc: "Camarão à provençal, alho e óleo, combinado ao arroz, risotos e massas são comuns nos menus locais.",
+            image: "/images/camarao.jpg",
+          },
+        ]}
+        buttonLabel="Ver Mais"
+      />
+
+      {/* Aqui entram os eventos */}
+      <EventosTimeline eventos={eventos} cor="#ec7702ff" />
 
       <Footer />
     </div>
