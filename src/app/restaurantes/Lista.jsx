8297920042011518a7ca5a53f04cc4ff6ccd258b
Mateus Rodrigo <mateus.rodrigo@aluno.ifsp.edu.br>
2025-11-18 17:45:16 -0300

    Mudanças do CSS 2

--- src/app/restaurantes/Lista.jsx
+++ src/app/restaurantes/Lista.jsx
@@ -1,24 +1,24 @@
-"use client";
-import Link from "next/link";
-import styles from "./Lista.module.css";
+// "use client";
+// import Link from "next/link";
+// import styles from "./Lista.module.css";
 
-export default function Lista() {
-  const restaurantes = [
-    { id: "mar-azul", nome: "Restaurante Mar Azul", tipo: "Frutos do Mar" },
-    // você pode adicionar mais aqui
-  ];
+// export default function Lista() {
+//   const restaurantes = [
+//     { id: "mar-azul", nome: "Restaurante Mar Azul", tipo: "Frutos do Mar" },
+//     // você pode adicionar mais aqui
+//   ];
 
-  return (
-    <div className={styles.grid}>
-      {restaurantes.map((r) => (
-        <Link key={r.id} href={`/restaurantes/${r.id}`}>
-          <div className={styles.card}>
-            <img src={`/images/${r.id}.png`} alt={r.nome} />
-            <h3>{r.nome}</h3>
-            <p>{r.tipo}</p>
-          </div>
-        </Link>
-      ))}
-    </div>
-  );
-}
+//   return (
+//     <div className={styles.grid}>
+//       {restaurantes.map((r) => (
+//         <Link key={r.id} href={`/restaurantes/${r.id}`}>
+//           <div className={styles.card}>
+//             <img src={`/images/${r.id}.png`} alt={r.nome} />
+//             <h3>{r.nome}</h3>
+//             <p>{r.tipo}</p>
+//           </div>
+//         </Link>
+//       ))}
+//     </div>
+//   );
+// }
